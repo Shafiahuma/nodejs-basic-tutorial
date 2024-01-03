3a212c8fa2f01cfd35ef975ea851644e8a486c3b
Shafia Nur Huma <snhuma2199@gmail.com>
2024-01-03 20:52:33 +0600

    Assignment 04 Done

--- assignment04-npm-practice/index.js
+++ assignment04-npm-practice/index.js
@@ -1,18 +1,23 @@
+
 // Import NPM Package
+const _ = require('underscore');
 
 // Call appropriate function from the NPM package to solve the assignment
+//_.contains() function from the underscore package.
+
+// Array to check
+const programmingLanguages = ["Java", "Javascript", "Typescript"];
 
 /**
  * Print out the result if ["Java", "Javascript", "Typescript"] this array 
  * contains "Java"
  */
 // First Answer 
-console.log("Replace this text with your answer");
-
+console.log(`Array contains "Java": ${_.contains(programmingLanguages, "Java")}`);
 
 /**
  * Print out the result if ["Java", "Javascript", "Typescript"] this array 
  * contains "C++"
  */
 // Second Answer
-console.log("Replace this text with your answer");
+console.log(`Array contains "C++": ${_.contains(programmingLanguages, "C++")}`);
